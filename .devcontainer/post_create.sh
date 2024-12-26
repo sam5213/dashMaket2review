81932a3e49b0ce338c53ab624a8c75bdb5ba44d4
sam5213 <gulmirazhb@gmail.com>
2024-12-26 15:00:00 +0000

    upd36

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -2,13 +2,13 @@
 
 # Выполняем команду из вашего файла YAML
 echo "Выполнение команды..."
-node server.js
+nohup python app.py &
 
 # Получаем URL Codespace
 codespace_url=$(codespace info --url)
 
 # Генерируем публичный URL
-public_url="http://example.com/?codespace_url=${codespace_url}"
+public_url="https://sam5213.github.io/dashMaket2review/?codespace_url=${codespace_url}"
 
 # Сохраняем публичный URL в файл
 echo $public_url > .devcontainer/public_url.txt
